3afa1d0c921ab837dbf00ff2b1c73c448d489300
BullChallenger <am23am45@naver.com>
2023-11-01 15:24:55 +0900

    Init Commit
    Add GitIgnore
    
    IntelliJ + all
    Gradle
    Java
    Windows

---
+++ src/main/java/com/example/whale/WhaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.whale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whale/WhaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
