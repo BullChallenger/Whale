8ddbebf693cb627ffe8bee9fb79a39d3ec09edbd
BullChallenger <am23am45@naver.com>
2023-11-09 19:18:17 +0900

    [REFACTOR] LoginController에 BaseController 적용

--- src/main/java/com/example/whale/controller/LoginController.java
+++ src/main/java/com/example/whale/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.example.whale.controller;
 
 import com.example.whale.dto.LoginDTO.LoginRequestDTO;
 import com.example.whale.dto.LoginDTO.LoginResponseDTO;
+import com.example.whale.dto.ResponseDTO;
 import com.example.whale.service.LoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class LoginController {
+public class LoginController extends BaseController {
 
     private final LoginService loginService;
 
     @PostMapping(value = "/login")
-    public ResponseEntity login(@RequestBody LoginRequestDTO dto) {
+    public ResponseDTO<LoginResponseDTO> login(@RequestBody LoginRequestDTO dto) {
         LoginResponseDTO result = loginService.login(dto);
-        return ResponseEntity.ok(result);
+        return ResponseDTO.ok(result);
     }
 
 }
