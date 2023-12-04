b4ac4fbbb7f57800143e6393e3027db397f62178
BullChallenger <am23am45@naver.com>
2023-12-04 16:47:12 +0900

    [FEAT] 로그인 성공 시 해당 유저 정보를 반환
    로그인 성공했을 때 해당 유저의 정보를 응답값으로 반환하도록 코드를
    추가했다

--- src/main/java/com/example/whale/security/handler/CustomLoginSuccessHandler.java
+++ src/main/java/com/example/whale/security/handler/CustomLoginSuccessHandler.java
@@ -2,19 +2,24 @@ package com.example.whale.security.handler;
 
 import com.example.whale.domain.RefreshToken;
 import com.example.whale.domain.UserEntity;
+import com.example.whale.dto.LoginDTO.LoginResponseDTO;
+import com.example.whale.dto.ResponseDTO;
 import com.example.whale.repository.RefreshTokenRepository;
 import com.example.whale.repository.UserRepository;
 import com.example.whale.security.provider.JwtProvider;
 import java.io.IOException;
-import java.util.Optional;
+import java.nio.charset.StandardCharsets;
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -25,6 +30,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
 
+    private final ObjectMapper objectMapper;
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
     private final RefreshTokenRepository refreshTokenRepository;
@@ -47,5 +53,14 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
         refreshTokenRepository.save(new RefreshToken(refreshToken, userEntity.getId()));
         ResponseCookie cookie = jwtProvider.setRefreshTokenInCookie(refreshToken);
         response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+
+        String loginSuccessUserInfo = objectMapper.writeValueAsString(ResponseDTO.ok(LoginResponseDTO.from(userEntity)));
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.getWriter().write(loginSuccessUserInfo);
+        response.getWriter().flush();
+        response.getWriter().close();
     }
+
 }
