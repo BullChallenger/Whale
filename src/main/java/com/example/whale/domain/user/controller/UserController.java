19b6222131bf1efbdb882e13ec414fcd81ecefde
BullChallenger <am23am45@naver.com>
2024-02-20 11:51:12 +0900

    [CHORE] Jenkins Build Test

--- src/main/java/com/example/whale/domain/user/controller/UserController.java
+++ src/main/java/com/example/whale/domain/user/controller/UserController.java
@@ -1,18 +1,27 @@
 package com.example.whale.domain.user.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.whale.domain.common.controller.BaseController;
-import com.example.whale.global.util.AuthenticationUtil;
 import com.example.whale.domain.common.dto.ResponseDTO;
-import com.example.whale.domain.user.model.AuthenticationUser;
 import com.example.whale.domain.user.dto.FindUserDTO.FindUserResponseDTO;
 import com.example.whale.domain.user.dto.SignUpDTO.SignUpRequestDTO;
 import com.example.whale.domain.user.dto.UpdateUserDTO.UpdateUserRequestDTO;
 import com.example.whale.domain.user.dto.UpdateUserDTO.UpdateUserResponseDTO;
+import com.example.whale.domain.user.model.AuthenticationUser;
 import com.example.whale.domain.user.service.UserService;
+import com.example.whale.global.util.AuthenticationUtil;
+
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -44,6 +53,10 @@ public class UserController extends BaseController {
     public ResponseDTO<Void> deleteUserById(@PathVariable("userId") Long userId) {
         userService.deleteUserById(userId);
         return ResponseDTO.ok();
+    }
+
+    public void test() {
+
     }
 
 }
